1887b97c29da4422fd0e52100555b96c4272473a
Unknown <1652510023@student.budiluhur.ac.id>
2018-09-03 11:02:46 +0700

    MQTT FILE

---
+++ MQTT/ESP8266/WIFISRIAL_SERVER_TELNET/fungtion.ino
@@ -0,0 +1,15 @@
+void subProses(){
+  if(incomingData == "ledOn"){
+     digitalWrite(BUILTIN_LED2, HIGH);
+     serverClient.print("LED OFF");
+  }
+  else if(incomingData == "ledOff"){
+    digitalWrite(BUILTIN_LED2, LOW);
+    serverClient.print("LED ON");
+  }
+  else if(incomingData == "temp"){
+    serverClient.print("30,5");
+  }
+  }
+  
+
