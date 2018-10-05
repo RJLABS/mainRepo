29a1adb10c899c6b0f3b2529fc722029d8bc49b2
Unknown <1652510023@student.budiluhur.ac.id>
2018-10-05 17:43:57 +0700

    PLC FILE

--- MQTT/181005-140040-esp01_1m/src/MQTT_func.ino
+++ MQTT/181005-140040-esp01_1m/src/MQTT_func.ino
@@ -14,3 +14,26 @@ void callback(char* topic, byte* payload, uint32_t length){//unsigned int length
     }
     buff="";
 }
+//===================================================
+void pub(String topic,String Data)
+{  
+  if(client.connected()){
+    char charData[120];
+    char charIP[50];
+    memset(charData,'\n',sizeof(charData));
+    memset(charIP,'\n',sizeof(charIP));
+    topic.toCharArray(charIP,topic.length()+1);
+    Data.toCharArray(charData,Data.length()+1);
+    client.publish(charIP,charData);
+    }   
+}
+
+//===========================================
+void sub(String Data1)
+{  
+  char charData1[50];
+  memset(charData1,'\n',sizeof(charData1));
+  Data1.toCharArray(charData1,Data1.length()+1);
+  client.subscribe(charData1);
+  //free(charData);
+}
